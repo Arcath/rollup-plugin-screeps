4c8344ef83e07d1512413cbf371dfa408a4e4d35
apemanzilla <dominic@marcuse.us>
2017-11-25 17:08:16 -0500

    Use rollup type definitions, don't mix named and default exports

--- src/rollup-plugin-screeps.ts
+++ src/rollup-plugin-screeps.ts
@@ -1,50 +1,33 @@
-import {ScreepsAPI} from 'screeps-api'
+import { ScreepsAPI } from 'screeps-api'
 import * as fs from 'fs'
 import * as git from 'git-rev-sync'
 import * as path from 'path'
+import { BundleOptions, Bundle, Plugin, GenerateOptions, WriteOptions } from 'rollup';
 
-export function screeps(screepsOptions: ScreepsOptions = {}){
-  return {
-    name: "screeps",
-    ongenerate: (options: any, bundle: any) => {
-      if(options.sourcemap){
-        generateSourceMaps(bundle)
-      }
-    },
-    onwrite: (options: any, bundle: any) => {
-      if(options.sourcemap){
-        writeSourceMaps(options)
-      }
+export function generateSourceMaps(bundle: Bundle) {
+  const b = bundle as Bundle & {map: { sourceContent: string }};
 
-      if(!screepsOptions.dryRun){
-        uploadSource(screepsOptions.configFile!, options, bundle)
-      }
-    }
-  }
-}
+  let tmp = b.map.toString
 
-export function generateSourceMaps(bundle: any){
-  let tmp = bundle.map.toString
+  delete b.map.sourceContent
 
-  delete bundle.map.sourceContent
-
-  bundle.map.toString = function () {
+  b.map.toString = function () {
     return "module.exports = " + tmp.apply(this, arguments) + ";";
   }
 }
 
-export function writeSourceMaps(options: any){
+export function writeSourceMaps(options: WriteOptions) {
   fs.renameSync(
     options.file + '.map',
     options.file + '.map.js'
   )
 }
 
-export function uploadSource(configFile: string, options: any, bundle: any){
-  if(!configFile){
+export function uploadSource(configFile: string, options: WriteOptions, bundle: Bundle) {
+  if (!configFile) {
     console.log('screeps() needs the path of your config file e.g. screeps({configFile: \'./screeps.json\'})')
     return
-  }else{
+  } else {
     let config = getConfig(configFile)
     let code = getFileList(options.file)
     let branch = getBranchName(config.branch)
@@ -52,13 +35,13 @@ export function uploadSource(configFile: string, options: any, bundle: any){
     let api = new ScreepsAPI()
 
     api.setServer(config)
-    api.auth().then(function(){
-      api.raw.user.branches().then((data: any) =>{
+    api.auth().then(function () {
+      api.raw.user.branches().then((data: any) => {
         let branches = data.list.map((b: any) => b.branch)
 
-        if(branches.includes(branch)){
+        if (branches.includes(branch)) {
           api.code.set(branch, code)
-        }else{
+        } else {
           api.raw.user.cloneBranch('', branch, code)
         }
       })
@@ -66,12 +49,12 @@ export function uploadSource(configFile: string, options: any, bundle: any){
   }
 }
 
-export function getConfig(configFile: string){
+export function getConfig(configFile: string) {
   let data = fs.readFileSync(configFile, 'utf8')
   return JSON.parse(data)
 }
 
-export function getFileList(outputFile: string){
+export function getFileList(outputFile: string) {
   let code: CodeList = {}
   let base = path.dirname(outputFile)
   let files = fs.readdirSync(base).filter((f) => path.extname(f) === '.js')
@@ -81,12 +64,28 @@ export function getFileList(outputFile: string){
   return code
 }
 
-export function getBranchName(branch: string){
-  if(branch === 'auto'){
+export function getBranchName(branch: string) {
+  if (branch === 'auto') {
     return git.branch()
-  }else{
+  } else {
     return branch
   }
 }
 
-export default screeps
+export function screeps(screepsOptions: ScreepsOptions = {}) {
+  return {
+    name: "screeps",
+
+    ongenerate(options: GenerateOptions, bundle: Bundle) {
+      if (options.sourcemap) generateSourceMaps(bundle);
+    },
+
+    onwrite(options: WriteOptions, bundle: Bundle) {
+      if (options.sourcemap) writeSourceMaps(options);
+
+      if (!screepsOptions.dryRun) uploadSource(screepsOptions.configFile!, options, bundle);
+    }
+  } as Plugin;
+}
+
+export default screeps;
