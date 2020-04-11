7c789a31815f172857c9619a9f183682fcaea3ad
Skyler Kehren <pyrodogg@gmail.com>
2020-04-11 09:30:04 +0300

    Rollup 2.0 update
    Contains breaking changes, compatible with rollup ^2.0.0
    
    Adds ES module build (dist/es/index.js) in addition to CommonJS
    (dist/index.js) build
    Replaces deprecated hook `ongenerate` with `generateBundle`.
    Replaces deprecated hook 'onwrite' with 'writeBundle'.
    Updates tests,
            For Rolllup 2.0 compatibility.
            Rewrites with async/await syntax.
            Moves test build dir to keep separate from test source
    Updates package dependencies.
    Standardizes order of attributes in `package.json`
    Reorganizes 3rd-party types so main and test buils can properly find them

--- typings/git-rev-sync/index.d.ts
+++ typings/git-rev-sync/index.d.ts
@@ -1,7 +1,3 @@
-declare module "screeps-api"{
-    const ScreepsAPI: any
-}
-
 declare module "git-rev-sync"{
     function remoteUrl(): string | undefined
     function branch(): string | undefined
